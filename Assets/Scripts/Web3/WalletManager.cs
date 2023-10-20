68f91e5acc83929a3547a9492268b3da5d6a905a
memxor <bhattacharyakun@gmail.com>
2023-10-20 16:30:03 +0530

    Added Wallet Manager, and using SuiPlay to authenticate users using Email/Password

---
+++ Assets/Scripts/Web3/WalletManager.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class WalletManager : MonoBehaviour
+{
+    public static WalletManager Instance { get; private set; }
+
+    [HideInInspector] public Suinet.SuiPlay.DTO.Player player = null;
+    [HideInInspector] public BigInteger playerSuiBalance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SetPlayer(Suinet.SuiPlay.DTO.Player player)
+    {
+        this.player = player;
+    }
+
+    public async Task<string> GetUserSuiBalance()
+    {
+        var walletAddress = player.Wallets.First().Value.Address;
+        var coinBalanceResult = await SuiApi.Client.GetBalanceAsync(walletAddress, null);
+        playerSuiBalance = coinBalanceResult.Result.TotalBalance;
+        return coinBalanceResult.Result.TotalBalance.ToString();
+    }
+}
